f684b0b7ebb7093323128655227897075573b468
coolfire02 <coolfire02@vortexnetwork.me>
2024-04-02 10:57:20 +0800

    Updated text color

--- components/specific/service-cards-list.tsx
+++ components/specific/service-cards-list.tsx
@@ -65,7 +65,7 @@ function ServiceCardList() {
             <h1 className={"text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold bg-clip-text text-transparent bg-[linear-gradient(to_right,theme(colors.green.300),theme(colors.orange.400),theme(colors.purple.400),theme(colors.yellow.200),theme(colors.purple.400),theme(colors.green.100),theme(colors.green.300))] bg-[length:200%_auto] animate-gradient"}>
                     Freelance Services
             </h1>
-            <div className="min-w-[100%] text-center  text-md sm:text-base md:text-lg pt-4 text-gray-300 flex items-center justify-center">
+            <div className="min-w-[100%] text-center  text-md sm:text-base md:text-lg pt-4 text-gray-100 flex items-center justify-center">
                 <div className="max-w-[80%] md:max-w-[50%]">
                     <p className="">Building products that stand out from the majority is what I'm passionate in. Work with me and we can discuss affordable renumerations as i build up my portfoilio.</p>
                 </div>
--- components/specific/skills-list.tsx
+++ components/specific/skills-list.tsx
@@ -19,7 +19,7 @@ function SkillsList() {
         <h1 className={"text-center text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold bg-clip-text text-transparent bg-[linear-gradient(to_right,theme(colors.green.300),theme(colors.orange.400),theme(colors.purple.400),theme(colors.yellow.200),theme(colors.purple.400),theme(colors.green.100),theme(colors.green.300))] bg-[length:200%_auto] animate-gradient"}>
                 Skills and Experience
         </h1>
-        <div className="min-w-[100%] text-center  text-md sm:text-base md:text-lg pt-4 text-gray-300 flex items-center justify-center">
+        <div className="min-w-[100%] text-center  text-md sm:text-base md:text-lg pt-4 text-gray-100 flex items-center justify-center">
             <div className="max-w-[80%] md:max-w-[50%]">
                 <p className="">View by category my experience in each field.</p>
             </div>
